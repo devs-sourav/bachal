01770daf8850c8ca8e4ac86e88022a80453a1a3d
devs-sourav <souravacharjee361@gmail.com>
2023-07-18 15:24:42 +0600

    netlify problem fixing commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import Grid from '@mui/material/Grid';
 
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='homeContainer'>
+      <Grid container className='mthome' spacing={2}>
+        <Grid item xs={4}>
+          1
+        </Grid>
+        <Grid item xs={4}>
+          2
+        </Grid>
+        <Grid item xs={4}>
+          3
+        </Grid>
+      </Grid>
+    </div>
+
   )
 }
 
